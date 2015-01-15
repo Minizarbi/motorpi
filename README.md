615fc10f5a5803937d363bbdd45e57074ff58ac2
Alexandre Lagane <minizarbi@hotmail.fr>
2015-01-15 16:28:43 +0000

    Ajout de l'aide et modification du README.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # motorpi
 Petit programme pour faire tourner mon moteur pas-à-pas sur mon Raspberry Pi.
+
+Le programme utilise la librairie WiringPi.
--- motorpi.c
+++ motorpi.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <string.h>
 
 #include <wiringPi.h>
 
+// Ces pins sont ceux que j'utilise, vous êtes libres de les changer.
 #define PIN1 5
 #define PIN2 6
 #define PIN3 10
@@ -19,6 +21,15 @@ void handler(int s) {
 }
 
 int main(int argc, char *argv[]) {
+	if (argc > 1 && !strcmp(argv[1], "--help")) {
+		printf("Utilisation :\n");
+		printf("\tmotorpi [secondes]\n");
+		printf("\tmotorpi --help\n");
+		printf("Si un nombre est rentré en paramètre, le moteur tourne pendant ce nombre de secondes puis le programme s'arrête.\n");
+		printf("Si aucun paramètre n'est fourni, le programme continue à tourner jusqu'à son interruption (^C).\n");
+		printf("Le programme nécessite normalement des droits d'administrateur pour utiliser les pins du Raspberry.\n");
+		return 0;
+	}
 	// Attente entre chaque pas en microsecondes.
 	int pause = 1000; 
 	// Compteur de pas.
